5c51b40f4a0f4f633794c1b897a975429211fc4f
Mehdi Sadeghi <msk1361@gmail.com>
2020-02-27 00:51:58 +0100

    Add html output.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,7 @@
 const standard = require('@neutrinojs/standardjs');
 const reactComponents = require('@neutrinojs/react-components');
 const jest = require('@neutrinojs/jest');
+const htmlTemplate = require('@neutrinojs/html-template');
 
 module.exports = {
   options: {
@@ -10,5 +11,18 @@ module.exports = {
     standard(),
     reactComponents(),
     jest(),
+    htmlTemplate({
+      // @neutrinojs/html-template includes a custom template that has more features
+      // (eg appMountId and lang support) than the default html-webpack-plugin template:
+      // https://github.com/jantimon/html-webpack-plugin/blob/master/default_index.ejs
+      template: require.resolve('@neutrinojs/html-template/template.ejs'),
+      appMountId: 'root',
+      lang: 'en',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1',
+      },
+      // Override pluginId to add an additional html-template plugin instance
+      pluginId: 'html',
+    }),
   ],
 };
