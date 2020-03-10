53b30dce8e52f46685091546cf3f221447a37a2e
Mehdi Sadeghi <msk1361@gmail.com>
2020-03-10 14:29:24 +0100

    Remove unnecessary dependency.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -2,7 +2,6 @@ const standard = require('@neutrinojs/standardjs');
 const reactComponents = require('@neutrinojs/react-components');
 const react = require('@neutrinojs/react');
 const jest = require('@neutrinojs/jest');
-const htmlTemplate = require('@neutrinojs/html-template');
 
 module.exports = {
   options: {
@@ -12,18 +11,5 @@ module.exports = {
     standard(),
     reactComponents(),
     jest(),
-    htmlTemplate({
-      // @neutrinojs/html-template includes a custom template that has more features
-      // (eg appMountId and lang support) than the default html-webpack-plugin template:
-      // https://github.com/jantimon/html-webpack-plugin/blob/master/default_index.ejs
-      template: require.resolve('@neutrinojs/html-template/template.ejs'),
-      appMountId: 'root',
-      lang: 'en',
-      meta: {
-        viewport: 'width=device-width, initial-scale=1',
-      },
-      // Override pluginId to add an additional html-template plugin instance
-      pluginId: 'html',
-    }),
   ],
 };
